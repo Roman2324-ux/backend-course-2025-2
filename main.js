97e709a1c9196d55d39d0a3c0172c465ad850b29
Roman bc2025-2 <seaaes231@gmail.com>
2025-09-22 20:42:28 +0300

    Code Bank URL

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+const currency = "usd"; const date_from = "20220115"; const date_to = "20220131";
+const base = new URL('https://bank.gov.ua/NBU_Exchange/exchange_site');
+base.searchParams.append('valcode', currency);
+base.searchParams.append('start', date_from);
+base.searchParams.append('end', date_to);
+base.searchParams.append('sort', 'exchangedate');
+base.searchParams.append('order', 'desc');
 
+base.searchParams.append('json', '');
+
+console.log(base.toString());
